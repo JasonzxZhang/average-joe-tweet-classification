c3108334a8e4ff477a6846a8fd215bb40fea94e6
steventhanna <steventhanna@gmail.com>
2018-04-11 14:37:37 -0400

    Added install script

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup
+
+setup(
+    name='the-average-joe',
+    version='0.1',
+    py_modules=['main'],
+    install_requires=[
+      'numpy',
+      'scipy',
+      'sklearn',
+      'nltk',
+      'aiohttp',
+      'aiodns',
+      'beautifulsoup4',
+      'cchardet',
+      'elasticsearch'
+    ],
+)
+
+
+# pip3 install git+https://git@github.com/ping/instagram_private_api.git@1.5.1
